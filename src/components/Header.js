111174e4b7ed1b10535f63db3a74886d64a9d988
Dean Elliott <dean@deanelliott.me>
2020-01-31 13:18:10 +0000

    Fix image paths

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,6 +7,13 @@ import 'static/css/style.css';
 
 import Logo from '../icons/Logo';
 
+import ogImage from '../static/images/social/git-basics-og-image.png';
+import cardImage from '../static/images/social/git-basics-twitter-card-image.png';
+
+import touchIcon from '../static/images/fav/apple-touch-icon.png';
+import icon32 from '../static/images/fav/favicon-32x32.png';
+import icon16 from '../static/images/fav/favicon-16x16.png';
+
 import styles from './Header.module.scss';
 
 export default function Header({ colorMode }) {
@@ -15,9 +22,7 @@ export default function Header({ colorMode }) {
     const siteTitle = 'Git Basics';
     const siteDesc = 'A little cheat sheet of common commands to help you become a Git pro.';
     const siteUrl = 'https://gitbasics.xyz';
-    const ogImg = 'images/social/git-basics-og-image.png';
-    const twitterImg = 'images/social/git-basics-twitter-card-image.png';
-    const twitterAcc = '@delliott85';
+    const twitterAcc = '@deadgooddean';
 
     const siteMeta = (
         <Helmet>
@@ -28,15 +33,15 @@ export default function Header({ colorMode }) {
             <meta name="robots" content="index,follow" />
             <meta name="googlebot" content="index,follow" />
 
-            <link rel="apple-touch-icon" sizes="180x180" href="images/fav/apple-touch-icon.png?v=1" />
-            <link rel="icon" type="image/png" sizes="32x32" href="images/fav/favicon-32x32.png?v=1" />
-            <link rel="icon" type="image/png" sizes="16x16" href="images/fav/favicon-16x16.png?v=1" />
+            <link rel="apple-touch-icon" sizes="180x180" href={touchIcon} />
+            <link rel="icon" type="image/png" sizes="32x32" href={icon32} />
+            <link rel="icon" type="image/png" sizes="16x16" href={icon16} />
 
-            <meta property="fb:app_id" content="123456789" />
+            <meta property="fb:app_id" content="497028727881181" />
             <meta property="og:url" content={siteUrl} />
             <meta property="og:type" content="website" />
             <meta property="og:title" content={siteTitle} />
-            <meta property="og:image" content={ogImg} />
+            <meta property="og:image" content={ogImage} />
             <meta property="og:image:alt" content={siteDesc} />
             <meta property="og:description" content={siteDesc} />
             <meta property="og:site_name" content={siteTitle} />
@@ -49,7 +54,7 @@ export default function Header({ colorMode }) {
             <meta name="twitter:url" content={siteUrl} />
             <meta name="twitter:title" content={siteTitle} />
             <meta name="twitter:description" content={siteDesc} />
-            <meta name="twitter:image" content={twitterImg} />
+            <meta name="twitter:image" content={cardImage} />
             <meta name="twitter:image:alt" content={siteDesc} />
 
         </Helmet>
