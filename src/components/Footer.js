b101086c2684e9229fe5e6738606a45ac89b823b
Dean Elliott <dean@deanelliott.me>
2020-02-09 17:21:27 +0000

    Add link to Github repo

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,6 +6,7 @@ export default function Footer() {
     return (
         <footer className={styles.footer}>
             <p>Built by <a href="https://deanelliott.me" target="_blank" rel="noopener noreferrer">Dean Elliott</a></p>
+            <p>View the repo on <a href="https://github.com/delliott85/gitbasics" target="_blank" rel="noopener noreferrer">Github</a></p>
         </footer>
     );
 }
